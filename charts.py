abdd75268e6364eb1e3321f71d195970a7d0874b
John Salas <johndsalas@gmail.com>
2021-04-29 15:07:02 -0500

    commiting notebook and pys to show charts

---
+++ charts.py
@@ -0,0 +1,92 @@
+
+import matplotlib.pyplot as plt
+
+def omnichart(df):
+    '''
+    Create a chart for each support showing the percentage of respondants for each value of a given column who
+    identified as needing that support.
+    '''
+
+    column_list = ['Household Income', 'Household Income: Above or Below $75K', 'Race',
+                   'Hispanic or Latinx', 'Survey Language', 'County', 'Virginia Health District', 
+                   'Nurture Area', 'Planning District 15', 'Pregnancy Stage']
+    
+    need_list = ['Birth Doula', 'Postpartum Doula', 'Lactation','Mental Health', 'Social', 'Diapers', 'Formula', 'Food', 'Housing',
+                 'Transportation', 'Health Insurance', 'Financial', 'COVID 19 Education','No Support']
+
+    removed_values_list = ['undisclosed']
+  
+    # get list of values that have 30 or more occurances in the column and set X axis to those values
+    for column in column_list:
+        
+        if column == 'Household Income':
+
+            value_list = ['40K and Under', '40K+ to 75K', '75K+ to 100K', '100K+ to 125K', 'Over 125K']
+
+        elif column == 'Household Income: Above or Below $75K':
+
+            value_list = ['Under $75K','Over $75K']
+
+        elif column == 'Pregnancy Stage':
+
+            value_list = ['2nd Trimester','3rd Trimester', '1 Year After Birth', '2 Years After Birth']
+
+        else:
+        
+            value_list = [value for value in set(df[f'{column}']) if df[f'{column}'].value_counts()[value] >= 25 and value not in removed_values_list]
+
+        X = [str(value) for value in value_list]
+
+        # get list of y values and plot chart
+        for need in need_list:
+
+            # y = rows where column equals value and need equals one divided by rows where column equals value 
+            y = [round((df[(df[f'{column}'] == value) & (df[f'{need}'] == 1)].shape[0] / df[df[f'{column}'] == value].shape[0])*100,0) for value in value_list]
+
+            plt.figure(figsize=(11, 6))
+            plt.bar(X, y, align='center', alpha=0.5)
+            plt.ylim([0,100])
+
+            plt.ylabel('% of Respondents in Each Catagory')
+            plt.title(f'Percent of Respondants Identifying {need} Among Most Helpful Supports by {column}')
+
+            plt.show()
+
+            output = [str(round(num)) + '%' for num in y]        
+            print(output)
+
+
+def get_hierarchy(df):
+
+    column_list = ['Household Income', 'Household Income: Above or Below $75K', 'Race',
+                   'Hispanic or Latinx', 'Survey Language', 'County', 'Virginia Health District', 
+                   'Nurture Area', 'Planning District 15', 'Pregnancy Stage']
+
+    need_list = ['No Support',   'Diapers', 'Formula','Food', 'Housing', 'Transportation', 'COVID 19 Education','Health Insurance',
+                 'Financial','Postpartum Doula', 'Birth Doula', 'Lactation', 'Mental Health', 'Social']
+
+    removed_values_list = ['undisclosed', False]
+
+    for column in column_list:
+
+        value_list = [value for value in set(df[f'{column}']) if df[f'{column}'].value_counts()[value] >= 25 and value not in removed_values_list]
+
+        X = need_list 
+
+        for value in value_list:
+
+            # respondants identifiying as value and chose need devided by respondants identifiying as value
+            y = [round((df[(df[column] == value) & (df[need] == 1)].shape[0] / df[df[column] == value].shape[0])*100,0) for need in need_list]
+
+            plt.barh(X,y)
+
+            plt.ylabel('Needs')
+            plt.xlabel('% of respondents Identifying Need as Among Most Helpful')
+            plt.title(f'Hiegharchy of Supports {column}: {value}')
+            plt.xlim([0,100])
+            plt.show()
+
+            output = [str(round(num)) + '%' for num in y]
+            output.reverse()
+
+            print(output)
---
+++ wrangle.py
@@ -0,0 +1,167 @@
+''' prepares qualitative data for statistical exploration '''
+
+import numpy as np
+import pandas as pd
+import re
+import unicodedata
+import pathlib
+
+import nltk
+from nltk.corpus import stopwords
+
+def get_data():
+
+    # acquiring data
+    df = pd.read_excel('final_data_set.xlsx')
+
+    return df
+
+def  income_values(value):
+    
+    if value == '$125,001 and over.': return "Over 125K"
+    elif value == 'S100,001 - $125,000': return "100K+ to 125K"
+    elif value == '$75,001 - $100,000': return "75K+ to 100K"
+    elif value == '$40,001 - $75,000': return "40K+ to 75K"
+    elif value == '$40,000 or under': return "40K and Under"
+    else: return  False
+
+def prep_data(df):
+
+    # filling nans with 0's
+    df = df.fillna(False)
+
+    # Make Stage column to hold Pregnancy stage
+    df['Pregnancy_Stage'] = 0
+    df['Pregnancy_Stage'][df['1stTrimester'] == '1stTrimester'] = '1st Trimester'
+    df['Pregnancy_Stage'][df['2ndTrimester'] == '2ndTrimester'] = '2nd Trimester'
+    df['Pregnancy_Stage'][df['3rdTrimester'] == '3rdTrimester'] = '3rd Trimester'
+    df['Pregnancy_Stage'][df['1yrBirth'] == '1yrBirth'] = '1 Year After Birth'
+    df['Pregnancy_Stage'][df['2yrBirth'] == '2yrBirth'] = '2 Years After Birth'
+    df['Pregnancy_Stage'][df['FamilyMember'] == 'FamilyMember'] = 'Family Member'
+
+    # drop unneeded columns
+    df = df[['Income', 'Over$75,001Income', 'RaceEthnicity', 'Are you of Hispanic or Latinx origin or descent?', 'SurveyLanguage', 
+             'County', 'VHD', 'NurtureArea', 'PlanningDist15',
+             'Pregnancy_Stage',
+             'BirthDoula', 'PostpartumDoula', 'Lactation', 'MentalHealth', 'Social', 'Diapers', 'Formula', 'Food', 
+             'Housing','Transportation', 'HealthInsurance', 'Financial', 'EducationCOVID19',
+             'NoSupport']]
+
+    # Rename columns for graphs
+    df = df.rename(columns={
+                            'Income' : 'Household Income',
+                            'Over$75,001Income' : 'Household Income: Above or Below $75K',
+                            'RaceEthnicity' : 'Race',
+                            'Are you of Hispanic or Latinx origin or descent?' : 'Hispanic or Latinx',
+                            'SurveyLanguage' : 'Survey Language',
+                            'VHD' : 'Virginia Health District',
+                            'NurtureArea' : 'Nurture Area',
+                            'PlanningDist15' : 'Planning District 15',
+                            'BirthDoula' : 'Birth Doula',
+                            'PostpartumDoula' : 'Postpartum Doula',
+                            'MentalHealth' : 'Mental Health',
+                            'HealthInsurance' : 'Health Insurance',
+                            'EducationCOVID19' : 'COVID 19 Education',
+                            'NoSupport' : 'No Support',
+                            'Pregnancy_Stage' : 'Pregnancy Stage'})
+
+    # rename values for the charts
+    df['Household Income: Above or Below $75K'] = df['Household Income: Above or Below $75K'].apply(lambda value: 'Over $75K' if value == 1 else ('Under $75K' if value == 0 else 'Undisclosed'))
+    df['Household Income'] = df['Household Income'].apply(income_values)
+    df['Nurture Area'] = df['Nurture Area'].apply(lambda value: 'In Area' if value == 1 else 'Out of Area')
+    df['Planning District 15'] = df['Planning District 15'].apply(lambda value: 'In Area' if value == 1 else 'Out of Area')
+
+    return df
+
+
+def get_qual_preped(df,col):
+    '''
+    Takes in cleaned survay data as a dataframe and a column name
+    returns a dataframe with only rows that have values in the column
+    the rows will preped for analysis
+    '''
+
+    df = df.dropna(subset=[col])
+    df[col] = df[col].apply(basic_clean).apply(lemmatize).apply(remove_stopwords)
+
+    return df[[col, 'race', 'income', 'pregnancy_stage']]
+
+def basic_clean(article):
+    '''
+    calls child functions preforms basic cleaning on a string
+    converts string to lowercase, ASCII characters,
+    and eliminates special characters
+    '''
+
+    # lowercases letters
+    article = article.lower()
+
+    # convert to ASCII characters
+    article = get_ASCII(article)
+
+    # remove non characters
+    article = purge_non_characters(article)
+    
+    return article
+
+def get_ASCII(article):
+    '''
+    normalizes a string into ASCII characters
+    '''
+
+    article = unicodedata.normalize('NFKD', article)\
+    .encode('ascii', 'ignore')\
+    .decode('utf-8', 'ignore')
+    
+    return article
+
+def purge_non_characters(article):
+    '''
+    removes special characters from a string
+    '''
+    
+    article = re.sub(r"[^a-z\s]", ' ', article)
+    
+    return article
+
+def remove_stopwords(article,extra_words=[],exclude_words=[]):
+    '''
+    removes stopwords from a string
+    user may specify a list of words to add or remove from the list of stopwords
+    '''
+
+    # create stopword list using english
+    stopword_list = stopwords.words('english')
+    
+    # remove words in extra_words from stopword list 
+    [stopword_list.remove(f'{word}') for word in extra_words]
+    
+    # add words fin exclude_words to stopword list
+    [stopword_list.append(f'{word}') for word in exclude_words]
+    
+    # slpit article into list of words
+    words = article.split()
+
+    # remove words in stopwords from  list of words
+    filtered_words = [w for w in words if w not in stopword_list]
+    
+    # rejoin list of words into article
+    article_without_stopwords = ' '.join(filtered_words)
+    
+    return article_without_stopwords
+
+def lemmatize(article):
+    '''
+    lemmatizes words in a string
+    '''
+
+    # create lemmatize object
+    wnl = nltk.stem.WordNetLemmatizer()
+    
+    # split article into list of words and stem each word
+    lemmas = [wnl.lemmatize(word) for word in article.split()]
+
+    #  join words in list into a string
+    article_lemmatized = ' '.join(lemmas)
+    
+    return article_lemmatized
